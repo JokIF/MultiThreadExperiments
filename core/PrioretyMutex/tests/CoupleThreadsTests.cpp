f595fe4b5aadefd3d3739d5529c3ec4c9555c22f
JokIF <jokifoki2@gmail.com>
2026-07-02 14:34:27 +0700

    Add last test with trylock for priotex. ------------------------------------------- Create ThreadLaucher and ThreadSignal prototypes. Thats Creates need to move in other subdir.

--- core/PrioretyMutex/tests/CoupleThreadsTests.cpp
+++ core/PrioretyMutex/tests/CoupleThreadsTests.cpp
@@ -70,3 +70,83 @@ TEST(PriotexTest, CorrectMultiThreadUsing)
     long long unsigned int expectedOverallSummation = iterTimesInMainLoop * 3 * 10; // 3 - threads count; 10 - sum value per iter 
     EXPECT_EQ(GetOverallSummation(), expectedOverallSummation);
 }
+
+TEST(PriotexTest, ThreadLauncher) //need to move in other subdir
+{
+    threadWorks::ThreadLauncher tl;
+    EXPECT_EQ(tl.GetThreadCount(), 0);
+    auto th1 = tl.CreateThread([] { for (size_t i = 0; i < 1000; i++); });
+    EXPECT_EQ(tl.GetThreadCount(), 1);
+    th1.join();
+    EXPECT_EQ(tl.GetThreadCount(), 0);
+}
+
+TEST(PriotexTest, TryLockMultiThreadUsing) //In doubt
+{
+    unsigned int GlobalCounter = 0;
+    unsigned int GlobalAltCounter = 0;
+
+    mutex::Priotex MainLoopPtx(3000);
+    mutex::Priotex SomeOperationPtx(200);
+    mutex::Priotex CounterPtx(10);
+
+    auto IncreaseCounter = [&CounterPtx, &GlobalCounter]
+    {
+        std::lock_guard lock(CounterPtx);
+        GlobalCounter++;
+    };
+
+    auto IncreaseAltCounter = [&CounterPtx, &GlobalAltCounter]
+    {
+        std::lock_guard lock(CounterPtx);
+        GlobalAltCounter++;
+    };
+
+    auto SomeOpWithCounter = [&SomeOperationPtx, &IncreaseCounter, &IncreaseAltCounter]
+    {
+        std::unique_lock lock(SomeOperationPtx, std::defer_lock);
+        if (lock.try_lock())
+            std::this_thread::sleep_for(5ms);
+        else
+            IncreaseAltCounter();
+
+        IncreaseCounter();
+    };
+    auto SomeOp = [&SomeOperationPtx]
+    {
+        std::lock_guard lock(SomeOperationPtx);
+        std::this_thread::sleep_for(5ms);
+    };
+
+
+    size_t IterTimes = 5;
+    auto MainLoopFirst = [IterTimes, &MainLoopPtx, &SomeOpWithCounter, &SomeOp]
+    {
+        for (size_t i = 0; i < IterTimes; i++) {
+            std::lock_guard lock(MainLoopPtx);
+            SomeOpWithCounter();
+        }
+    };
+
+    auto MainLoopSecond = [IterTimes, &MainLoopPtx, &SomeOpWithCounter, &IncreaseCounter, &SomeOp]
+    {
+        std::unique_lock lock(MainLoopPtx);
+        for (size_t i = 0; i < IterTimes; i++) {
+            lock.unlock();
+            SomeOp();
+            lock.lock();
+        }
+    };
+    
+    {
+        std::jthread th4(MainLoopSecond);
+        std::jthread th5(MainLoopSecond);
+
+
+        std::jthread th1(MainLoopFirst);
+        std::jthread th2(MainLoopFirst);
+        std::jthread th3(MainLoopFirst);
+    }
+    
+    EXPECT_EQ(GlobalCounter, GlobalAltCounter);
+}
