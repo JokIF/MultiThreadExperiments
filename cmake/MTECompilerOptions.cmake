c14722d00b9834bf95b1e31be0cfc7f07d0901e8
JokIF <jokifoki2@gmail.com>
2026-07-28 21:44:47 +0700

    Update builder settings. Remove all "project" title from all subdirs. Create MTECompilerOptions.cmake to add compile settings at build Create MTEUtilities.cmake to shorten CMakeLists.txt files

---
+++ cmake/MTECompilerOptions.cmake
@@ -0,0 +1,41 @@
+add_library(MteCompileOptions INTERFACE)
+
+target_compile_options(MteCompileOptions INTERFACE
+    $<$<CXX_COMPILER_ID:MSVC>:
+        /W4
+        /permissive-
+        /Zc:preprocessor
+        /Zc:__cplusplus
+        /EHsc
+        /MP
+        /sdl
+        /volatile:iso
+        /diagnostics:caret
+        /utf-8
+    >
+
+    $<$<CXX_COMPILER_ID:GNU>:
+        -Wall -Wextra -Wpedantic
+        -Wshadow -Wnon-virtual-dtor -Wold-style-cast
+        -Wcast-align -Wunused -Woverloaded-virtual
+        -Wconversion -Wsign-conversion -Wnull-dereference
+        -Wdouble-promotion -Wformat=2 -Wmissing-declarations
+        -Wredundant-decls -Wundef -Wstrict-aliasing
+        -Wlogical-op
+        -fconcepts-diagnostics-depth=0
+    >
+
+    $<$<CXX_COMPILER_ID:Clang,AppleClang>:
+        -Wall -Wextra -Wpedantic
+        -Wshadow -Wnon-virtual-dtor -Wold-style-cast
+        -Wcast-align -Wunused -Woverloaded-virtual
+        -Wconversion -Wsign-conversion -Wnull-dereference
+        -Wdouble-promotion -Wformat=2 -Wmissing-declarations
+        -Wredundant-decls -Wundef -Wstrict-aliasing
+        -Wthread-safety -Wthread-safety-beta -Wmove
+    >
+)
+
+target_link_options(MteCompileOptions INTERFACE
+    $<$<CXX_COMPILER_ID:MSVC>:/guard:cf>
+)
---
+++ cmake/MTEUtilities.cmake
@@ -0,0 +1,44 @@
+function(add_mte_example_with_settings TARGET_NAME)
+    add_executable(${TARGET_NAME} ${ARGN})
+    target_link_libraries(${TARGET_NAME} PRIVATE MteCompileOptions)
+endfunction()
+
+function(add_mte_interface_library_with_settings TARGET_NAME)
+    add_library(${TARGET_NAME} INTERFACE)
+    
+    target_include_directories(${TARGET_NAME}
+        INTERFACE
+            $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+    )
+
+    target_link_libraries(${TARGET_NAME} 
+        INTERFACE 
+            MteCompileOptions
+    )
+endfunction()
+
+function(add_mte_public_library_with_settings TARGET_NAME)
+    add_library(${TARGET_NAME} ${ARGN})
+    
+    target_include_directories(${TARGET_NAME}
+        PUBLIC
+            $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+    )
+
+    target_link_libraries(${TARGET_NAME} 
+        PUBLIC 
+            MteCompileOptions
+    )
+endfunction()
+
+function(add_mte_gtest_executable TARGET_NAME LIBS_NAMES)
+    add_executable(${TARGET_NAME} ${ARGN})
+    target_link_libraries(${TARGET_NAME} 
+        PRIVATE 
+            gtest_main
+            MteCompileOptions
+            ${LIBS_NAMES}
+    )
+    target_compile_definitions(${TARGET_NAME} PRIVATE BUILD_TESTING)
+    add_test(NAME ${TARGET_NAME} COMMAND ${TARGET_NAME})
+endfunction()
