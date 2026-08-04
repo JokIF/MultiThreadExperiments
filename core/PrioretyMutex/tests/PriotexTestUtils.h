699ce9637f30bed880e386d0fcc4edb63eaae54c
JokIF <jokifoki2@gmail.com>
2026-08-04 12:07:13 +0700

    Fix warnings

--- core/PrioretyMutex/tests/PriotexTestUtils.h
+++ core/PrioretyMutex/tests/PriotexTestUtils.h
@@ -6,7 +6,7 @@ namespace unitTests
 class UnitTestDataPriotex
 {
 public:
-    explicit UnitTestDataPriotex(const mutex::Priotex& ptx) : ptx(ptx) {}
+    explicit UnitTestDataPriotex(const mutex::Priotex& ptx_) : ptx(ptx_) {}
 
     mutex::Priotex::Priority GetPriotexPriority() const { return ptx.m_priorityValue; }
     mutex::Priotex::Priority GetLastPriority() const { return ptx.m_prevPriorityValue; }
